b94935940c33729b2692f684fc2cde5bd6519880
matrocheetos <58711427+matrocheetos@users.noreply.github.com>
2023-09-12 00:35:37 -0300

    add mqtt class

--- src/servidor.py
+++ src/servidor.py
@@ -1,5 +1,6 @@
 from concurrent import futures
 
+import paho.mqtt.client as mqtt
 import grpc
 
 import interface_pb2  # Import the generated protobuf Python code
@@ -287,6 +288,40 @@ class KeyValueStoreServicer(interface_pb2_grpc.KeyValueStoreServicer):
             context.set_details(str(e.details()))
             raise grpc.RpcError
 
+# paho.mqtt.python client class example
+class mqttClient(mqtt.Client):
+
+    def on_connect(self, mqttc, obj, flags, rc):
+        print("rc: "+str(rc))
+
+    def on_connect_fail(self, mqttc, obj):
+        print("Connect failed")
+
+    def on_message(self, mqttc, obj, msg):
+        print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+    def on_publish(self, mqttc, obj, mid):
+        print("mid: "+str(mid))
+
+    def on_subscribe(self, mqttc, obj, mid, granted_qos):
+        print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+    def on_log(self, mqttc, obj, level, string):
+        print(string)
+
+    def run(self):
+        self.connect("127.0.0.1", 1883, 60)
+        self.subscribe("projeto-sd", 2)
+
+        rc = 0
+        while rc == 0:
+            rc = self.loop()
+        return rc
+
+def mqtt_connect():
+    mqttc = mqttClient()
+    rc = mqttc.run()
+    print("rc: "+str(rc))
 
 def serve(port: int = 50051):
     try:
@@ -304,4 +339,5 @@ def serve(port: int = 50051):
 
 
 if __name__ == '__main__':
+    mqtt_connect()
     serve()
