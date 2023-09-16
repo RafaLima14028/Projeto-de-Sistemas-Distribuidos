36f66577ed6c74b5085ee3dfb7b4777fc81cd808
matrocheetos <58711427+matrocheetos@users.noreply.github.com>
2023-09-16 20:30:39 -0300

    add client.sh, compile.sh & server.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+python -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
-python -m grpc_tools.protoc -I./src --python_out=./src --grpc_python_out=./src src/interface.proto
+python3 -m grpc_tools.protoc -I./src --python_out=./src --grpc_python_out=./src src/interface.proto
